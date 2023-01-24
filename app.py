a81ffcfd7d6515855992ac93e1095b68a3c42635
albarqh21 <123430283+albarqh21@users.noreply.github.com>
2023-01-24 12:45:44 +0700

    aaa

---
+++ app.py
@@ -0,0 +1,11 @@
+from app import app
+
+# python -m venv env 1
+
+# env\scripts\activate 2
+# set FLASK_APP=app.py
+# set FLASK_DEBUG=1
+# flask run
+
+# pip freeze > requirements.txt
+# pip install -r requirements.txt 3
